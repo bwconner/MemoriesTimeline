36bf4b8f2109deaa54ea399ff068ec5325165341
Bruce Conner <bruce.conner.ii@gmail.com>
2018-02-07 23:15:38 -0500

    Memories Timeline - View Media Pages
    Unstyled shells for pages to view specific images or videos. Currently
    just has a back button and grabs an img src to display based on the
    image id passed in the url.

--- src/components/view-picture.js
+++ src/components/view-picture.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
+import {connect} from "react-redux";
 import {Link} from "react-router-dom";
 
-export default class ViewPicture extends Component {
-  render() {
-	return (
-		<div>
-			<Link to="/" >
-				Return to Search
-			</Link>
-			View Picture Page
-		</div>
-	);
-  }
+class ViewPicture extends Component {
+
+	render() {
+		const {props} = this.props;
+		const imageId = this.props.match.params.id;
+		const imageSrc = props.videos.filter(image => image.id === imageId);
+
+		return (
+			<div>
+				<Link to="/" >
+					Return to Search
+				</Link>
+				View Picture Page
+				<img src={imageSrc[0].filepath}/>
+			</div>
+		);
+	}
 }
+
+function mapStateToProps(props, ownProps) {
+	return{props};
+}
+
+export default connect(mapStateToProps)(ViewPicture);
--- src/components/view-video.js
+++ src/components/view-video.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
+import {connect} from "react-redux";
 import {Link} from "react-router-dom";
 
-export default class ViewVideo extends Component {
-  render() {
-	return (
-		<div>
-			<Link to="/" >
-				Return to Search
-			</Link>
-			View Image Page
-		</div>
-	);
-  }
+class ViewVideo extends Component {
+
+	render() {
+		const {props} = this.props;
+		const imageId = this.props.match.params.id;
+		const imageSrc = props.pictures.filter(image => image.id === imageId);
+
+		return (
+			<div>
+				<Link to="/" >
+					Return to Search
+				</Link>
+				View Video Page
+				<img src={imageSrc[0].filepath}/>
+			</div>
+		);
+	}
 }
+
+function mapStateToProps(props, ownProps) {
+	return{props};
+}
+
+export default connect(mapStateToProps)(ViewVideo);
