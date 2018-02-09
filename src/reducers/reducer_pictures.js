8ba2f4d25ea46c6b752cabe3a0ab6929045ceac0
Bruce Conner <bruce.conner.ii@gmail.com>
2018-02-08 23:07:53 -0500

    Memories Timeline - Test Data
    Updating test data structure.

--- src/reducers/reducer_pictures.js
+++ src/reducers/reducer_pictures.js
@@ -1,8 +1,8 @@
 export default function() {
 	return [
-		{caption: 'Christmas', year: '1989', filepath:'../../media/images/test1.jpg', id:'123'},
-		{caption: 'Birthday', year: '1995', filepath:'../../media/images/test2.jpg', id:'234'},
-		{caption: 'School', year: '2001', filepath:'../../media/images/test3.jpg', id:'345'},
-		{caption: 'College', year: '2012', filepath:'../../media/images/test4.jpg', id:'456'}
+		{title: 'Christmas', year: '1989', caption: 'This is where a description will go...',filepath:'../../media/images/test1.jpg', id:'123'},
+		{title: 'Birthday', year: '1995', caption: 'This is where a description will go...',filepath:'../../media/images/test2.jpg', id:'234'},
+		{title: 'School', year: '2001', caption: 'This is where a description will go...',filepath:'../../media/images/test3.jpg', id:'345'},
+		{title: 'College', year: '2012', caption: 'This is where a description will go...',filepath:'../../media/images/test4.jpg', id:'456'}
 	]
 }
