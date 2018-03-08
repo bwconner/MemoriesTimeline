1076c5e6c88c4dbb09231cc671964c51475eef16
Bruce Conner <bruce.conner.ii@gmail.com>
2018-03-07 20:31:28 -0500

    Updating Webpack to use SCSS
    Updating my webpack config to use scss styles.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   entry: [
-    './src/index.js'
+    './src/index.js',
+    './style/style.scss'
   ],
   output: {
     path: __dirname,
@@ -8,13 +9,19 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      },
+      {
+          test: /\.scss$/,
+          loaders: ["style-loader","css-loader","sass-loader"]
       }
-    }]
+    ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
