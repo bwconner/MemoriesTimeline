dd419825185d0846da07d505c210bd82e1ecbf52
Bruce Conner <bruce.conner.ii@gmail.com>
2018-07-01 18:05:37 -0700

    Reducers for Testing
    Reducers with hard coded arrays of data for testing of images and videos

--- src/reducers/reducer_pictures.js
+++ src/reducers/reducer_pictures.js
@@ -1,8 +1,21 @@
 export default function() {
 	return [
-		{title: 'Christmas', year: '1989', caption: 'This is where a description will go...',filepath:'../../media/images/test1.jpg', id:'123'},
-		{title: 'Birthday', year: '1995', caption: 'This is where a description will go...',filepath:'../../media/images/test2.jpg', id:'234'},
-		{title: 'School', year: '2001', caption: 'This is where a description will go...',filepath:'../../media/images/test3.jpg', id:'345'},
-		{title: 'College', year: '2012', caption: 'This is where a description will go...',filepath:'../../media/images/test4.jpg', id:'456'}
+		{title: 'Christmas', year: '1989', caption: 'This is where a description will go...',filepath:'../../media/images/test1.jpg', id:'1'},
+		{title: 'Birthday', year: '1995', caption: 'This is where a description will go...',filepath:'../../media/images/test2.jpg', id:'2'},
+		{title: 'School', year: '2001', caption: 'This is where a description will go...',filepath:'../../media/images/test3.jpg', id:'3'},
+		{title: 'College', year: '2012', caption: 'This is where a description will go...',filepath:'../../media/images/test4.jpg', id:'4'},
+		{title: 'Christmas', year: '1963', caption: 'This is where a description will go...',filepath:'../../media/images/test5.jpg', id:'5'},
+		{title: 'Birthday', year: '1998', caption: 'This is where a description will go...',filepath:'../../media/images/test6.jpg', id:'6'},
+		{title: 'School', year: '2004', caption: 'This is where a description will go...',filepath:'../../media/images/test7.jpg', id:'7'},
+		{title: 'Christmas', year: '1988', caption: 'This is where a description will go...',filepath:'../../media/images/test8.jpg', id:'8'},
+		{title: 'Birthday', year: '2018', caption: 'This is where a description will go...',filepath:'../../media/images/test9.jpg', id:'9'},
+		{title: 'School', year: '2006', caption: 'This is where a description will go...',filepath:'../../media/images/test10.jpg', id:'10'},
+		{title: 'Christmas', year: '1994', caption: 'This is where a description will go...',filepath:'../../media/images/test11.jpg', id:'11'},
+		{title: 'Birthday', year: '2003', caption: 'This is where a description will go...',filepath:'../../media/images/test12.jpg', id:'12'},
+		{title: 'School', year: '1974', caption: 'This is where a description will go...',filepath:'../../media/images/test13.jpg', id:'13'},
+		{title: 'Christmas', year: '1967', caption: 'This is where a description will go...',filepath:'../../media/images/test14.jpg', id:'14'},
+		{title: 'Birthday', year: '1995', caption: 'This is where a description will go...',filepath:'../../media/images/test15.jpg', id:'15'},
+		{title: 'School', year: '2001', caption: 'This is where a description will go...',filepath:'../../media/images/test16.jpg', id:'16'},
+		{title: 'Christmas', year: '1989', caption: 'This is where a description will go...',filepath:'../../media/images/test17.jpg', id:'17'}
 	]
 }
--- src/reducers/reducer_videos.js
+++ src/reducers/reducer_videos.js
@@ -1,8 +1,16 @@
 export default function() {
 	return [
-		{title: 'Christmas', year: '1989', caption: 'This is where a description will go...',filepath:'../../media/images/test1.jpg', id:'123'},
-		{title: 'Birthday', year: '1995', caption: 'This is where a description will go...',filepath:'../../media/images/test2.jpg', id:'234'},
-		{title: 'School', year: '2001', caption: 'This is where a description will go...',filepath:'../../media/images/test3.jpg', id:'345'},
-		{title: 'College', year: '2012', caption: 'This is where a description will go...',filepath:'../../media/images/test4.jpg', id:'456'}
+		{title: 'A&F', year: '2014', caption: 'This is where a description will go...',filepath:'../../media/videos/test1.jpg', id:'1'},
+		{title: 'Helicopter', year: '2012', caption: 'This is where a description will go...',filepath:'../../media/videos/test2.jpg', id:'2'},
+		{title: 'Halloween', year: '2013', caption: 'This is where a description will go...',filepath:'../../media/videos/test3.jpg', id:'3'},
+		{title: 'Vegas', year: '2009', caption: 'This is where a description will go...',filepath:'../../media/videos/test4.jpg', id:'4'},
+		{title: 'Mae', year: '1989', caption: 'This is where a description will go...',filepath:'../../media/videos/test5.jpg', id:'5'},
+		{title: 'JFK', year: '1977', caption: 'This is where a description will go...',filepath:'../../media/videos/test6.jpg', id:'6'},
+		{title: 'School', year: '2001', caption: 'This is where a description will go...',filepath:'../../media/videos/test7.jpg', id:'7'},
+		{title: 'Christmas', year: '1973', caption: 'This is where a description will go...',filepath:'../../media/videos/test8.jpg', id:'8'},
+		{title: 'Birthday', year: '1995', caption: 'This is where a description will go...',filepath:'../../media/videos/test9.jpg', id:'9'},
+		{title: 'Avery', year: '2001', caption: 'This is where a description will go...',filepath:'../../media/videos/test10.jpg', id:'10'},
+		{title: 'Wedding', year: '1987', caption: 'This is where a description will go...',filepath:'../../media/videos/test11.jpg', id:'11'},
+		{title: 'Birthday', year: '2003', caption: 'This is where a description will go...',filepath:'../../media/videos/test12.jpg', id:'12'}
 	]
 }
